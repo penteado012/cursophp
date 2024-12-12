c7133850deecdd5b6c47549ce72a495a6c1a32e3
Matheus Penteado <matheuspent12@gmail.com>
2024-12-12 14:28:59 -0300

    Initial commit

---
+++ ex003/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos primitivos em PHP</h1>
+    <?php
+    //0x = Hexadecimal || 0b = Binário || 0 = Octal
+    //$num = 010; 
+    //echo "O valor da variável é $num";
+
+    // $v = "Gustavo";
+    // var_dump($v);
+
+    // $num = (integer) 3e2; // 3 x 10(2) coerção
+    // echo "O valor é $num";
+    //var_dump($num);
+
+    // $num = (double) "950";
+    // var_dump($num); 
+
+    // $casado = true;
+    // var_dump($casado);
+    // print "O valor pra casado é $casado";
+
+    // $vet = [6, 2, "Maria", 3, false];
+    // var_dump($vet);
+
+    // class Pessoa {
+    //     private string $nome;
+    // }
+
+    // $p = new Pessoa;
+    // var_dump($p);
+
+    ?>
+</body>
+</html>
